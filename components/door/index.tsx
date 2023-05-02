876c16a8652f5886a85396127bd642422a4fd9ed
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2023-05-02 00:17:01 -0300

    add door handle

--- components/door/index.tsx
+++ components/door/index.tsx
@@ -6,6 +6,7 @@ const Door = () => {
       <div className={styles.structure}>
         <div className={styles.door}>
           <div className={styles.number}>3</div>
+          <div className={styles.handle}></div>
         </div>
       </div>
       <div className={styles.floor}></div>
